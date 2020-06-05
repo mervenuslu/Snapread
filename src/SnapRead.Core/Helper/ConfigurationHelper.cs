64fb48e84285e74c822eadcc14dde5bad56dc66a
Gökten Karadağ <gktnkrdg@gmail.com>
2020-06-05 07:59:24 +0300

    Add configuration helper

---
+++ src/SnapRead.Core/Helper/ConfigurationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapRead.Core.Helper
+{
+    public static class ConfigurationHelper
+    {
+        public static IConfiguration GetConfig()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            return builder.Build();
+        }   
+
+      
+    }
+   
+}
