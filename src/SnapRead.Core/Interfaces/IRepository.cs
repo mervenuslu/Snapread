03d7e05d575b46e71b7187273e2abbe733696e48
Gökten Karadağ <gktnkrdg@gmail.com>
2020-06-05 07:58:22 +0300

    Add repository interface

---
+++ src/SnapRead.Core/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using SnapRead.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapRead.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
